ea1016a776d89b83f4d9fd614b4f877aaea59384
VicTech Electronics <victechelectronics@gmail.com>
2023-06-06 21:58:14 +0300

    Rehema

--- codes/codes.ino
+++ codes/codes.ino
@@ -10,13 +10,18 @@
 void setup() {
   pinMode(switch_pin, OUTPUT);
   pinMode(voltage_pin, INPUT);
+  pinMode(backlight, OUTPUT);
 
   Serial.begin(9600);
+  lcd.begin(16, 2);
 
   servo.attach(servo_pin);
   servo.write(0);
   EEPROM.get(timeAddress, time);
   EEPROM.get(sizeAddress, size);
+
+  digitalWrite(backlight, HIGH);
+  lcdPrint("Welcome", ""); delay(3e3);
 
   attachInterrupt(digitalPinToInterrupt(btn1_pin), settingsInterrupt, FALLING);
   attachInterrupt(digitalPinToInterrupt(btn2_pin), confirmationInterrupt, FALLING);
