c9301ec598d0f56d5ddc7b67e3c2628c5e1067c7
VicTech Electronics <victechelectronics@gmail.com>
2023-06-08 00:29:08 +0300

    Control car

---
+++ VictoniX codes/incomplete/Communication.h
@@ -0,0 +1,62 @@
+#include <SoftwareSerial.h>
+#include <Arduino_JSON.h>
+#include <TinyGPS++.h>
+
+#define ACCIDENT  "detected"
+
+// Objects
+SoftwareSerial serial(9, 10);
+SoftwareSerial serialBT(6, 7);
+SoftwareSerial serialGPS(2, 3);
+
+TinyGPSPlus gps;
+JSONVar jsonObject;
+
+// Decralation of usefull variables
+String serial_data, location;
+float latitudes[5] = {-6.81386, -6.81425, -6.81437, -6.81374, -6.81423};
+float longitudes[5] = {39.28015, 39.28001, 39.27932, 39.28235, 39.28005};
+
+
+// Method to read GPS locattion
+String getLocation(){
+  serialGPS.listen();
+  float latitude, longitude;
+  uint8_t random_selection = random(0, 5);
+
+  if(gps.encode(serialGPS.read())){
+    if(gps.location.isValid()){
+      latitude = gps.location.lat();
+      longitude = gps.location.lng();
+    }else{
+      latitude = latitudes[random_selection];
+      longitude = longitudes[random_selection];
+    }
+  }else{
+    latitude = latitudes[random_selection];
+    longitude = longitudes[random_selection];
+  }
+
+  jsonObject["latitude"] = latitude;
+  jsonObject["longitude"] = longitude;
+  return JSON.stringify(jsonObject);
+}
+
+// Method to listen to serial data
+void listenSerial(){
+  serial.listen();
+  if(serial.isListening()){
+    while(serial.available()){
+      serial_data = serial.readString();
+      serial_data.trim();
+      serial_data.toLowerCase();
+    }
+  }
+
+  if(serial_data == ACCIDENT){
+    location = getLocation();
+    serial.listen();
+    serial.println(location);
+  }
+  serialBT.listen();
+}
--- VictoniX codes/incomplete/incomplete.ino
+++ VictoniX codes/incomplete/incomplete.ino
@@ -1,49 +1,10 @@
-#include <SoftwareSerial.h>
+/*
+  VicTech Electronics
+  VictoniX final year project
+  FYP2022/23
+*/
 
-SoftwareSerial serial();
-SoftwareSerial serialBT(10, 11);
-
-// Definition of useful pins
-const uint8_t enabel_left=1, enabel_right=24, left1=0, left2=2, right1=25, right2=26;
-
-// Declaration of useful variables
-char data;
-
-//////////// Custome functions //////////////
-// Function to listen the data from the serialBT
-char listenBT(){
-  if(serialBT.available()){
-    Serial.println("Collect data");
-    data = serialBT.read();
-    Serial.print("Data ");
-    Serial.println(data);
-    return data;
-  } return 'n';
-}
-
-// Function to control car wheels
-void whilesControl(bool en_left, bool en_right, bool l1, bool l2, bool r1, bool r2){
-  digitalWrite(enabel_left, en_left);
-  digitalWrite(enabel_right, en_right);
-  digitalWrite(left1, l1);
-  digitalWrite(left2, l2);
-  digitalWrite(right1, r1);
-  digitalWrite(right2, r2);
-}
-
-
-// Function to control car motion
-void motionControl(){
-  data = listenBT();
-  if(data == 'F') whilesControl(true, true, true, false, true, false);
-  else if(data == 'B') whilesControl(true, true, false, true, false, true);
-  else if(data == 'L') whilesControl(false, true, true, false, true, false);
-  else if(data == 'R') whilesControl(true, false, true, false, true, false);
-  else if(data == 'S') whilesControl(false, false, true, false, true, false);
-  else if(data == 'V') serial.println("horn_on");
-  else if(data == 'v') serial.println("horn_off");
-}
-
+#include "Control.h"
 
 //////////// Default function ///////////////////
 void setup() {
@@ -57,10 +18,13 @@ void setup() {
   serial.begin(9600);
   Serial.begin(9600);
   serialBT.begin(9600);
+  serialGPS.begin(9600);
 
   Serial.println("Program start");
 }
 
 void loop() {
   motionControl();
+  listenSerial();
+  delay(100);
 }
