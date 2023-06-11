0c1e748b4727cb3c2378bfdc584e95fc7c55bbda
VicTech Electronics <victechelectronics@gmail.com>
2023-06-11 18:59:36 +0300

    Done

--- codes/codes.ino
+++ codes/codes.ino
@@ -11,16 +11,22 @@
 #define d5 11
 #define d6 12
 #define d7 13
-#define trig 6
-#define echo 7
 
 // Initialize the objects
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-UltraSonicDistanceSensor ultrasonic(trig, echo);
+// UltraSonicDistanceSensor ultrasonic(trig, echo);
 
 // Decralation of other useful pins
-const uint8_t buzzer_pin = 5, btn_pin = 3,
+const uint8_t buzzer_pin = 5, btn_pin = 3, trig_pin=6, echo_pin=7,
               indicator_pin = A1, backlight_pin = A0;
+
+// Decralation of useful variables
+boolean measure = false;
+float distance=0.0;
+
+// const int numSamples = 10;
+// int samples[numSamples];
+// int currentIndex = 0;
 
 
 ////////////////* Custom methods (Functions) */////////////////////////
@@ -43,16 +49,33 @@ String cut_string(String str, int col) {
   return str.substring(0, col);
 }
 
-// Decralation of useful variables
-boolean measure = false;
-float distance;
+
 
 // Method to handle state Interrupt
 void state(){
-  measure = !measure;
+  measure = true;
   digitalWrite(buzzer_pin, HIGH);
-  delayMicroseconds(15e3);
+  delayMicroseconds(15000);
   digitalWrite(buzzer_pin, LOW);
+  delayMicroseconds(15000);
+}
+
+// Method to distance
+float getDistance(){
+  // Send a short pulse to the trigger pin
+  digitalWrite(trig_pin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig_pin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig_pin, LOW);
+
+  // Read the duration of the pulse from the echo pin
+  long duration = pulseIn(echo_pin, HIGH);
+
+  // Calculate the distance in centimeters
+  // Speed of sound is approximately 343 meters per second
+  // Divide by 2 to account for round trip travel of the sound wave
+  return duration * 0.0343 / 2;
 }
 
 
@@ -63,22 +86,31 @@ void setup() {
   pinMode(btn_pin, INPUT_PULLUP);
   pinMode(indicator_pin, OUTPUT);
   pinMode(backlight_pin, OUTPUT);
+  pinMode(trig_pin, OUTPUT);
+  pinMode(echo_pin, INPUT);
 
   Serial.begin(9600);
   lcd.begin(16, 2);
 
+  digitalWrite(backlight_pin, HIGH);
   attachInterrupt(digitalPinToInterrupt(btn_pin), state, FALLING);
 }
 
 void loop() {
   if(measure) {
-    distance = ultrasonic.measureDistanceCm();
+    distance = 0.0;
+    for(int i=0; i<100; i++){
+      distance += getDistance();
+      delay(10);
+    }
+    distance = distance / 100.0;
     digitalWrite(indicator_pin, HIGH);
-    digitalWrite(backlight_pin, HIGH);
-  }else{
+    lcd_print("Distance Measure", "Dist: " + String(distance) + " cm.");
+    delay(3e3);
+    measure = false;
+  }else
     digitalWrite(indicator_pin, LOW);
-    digitalWrite(backlight_pin, LOW);
-  }
+  
 
   lcd_print("Distance Measure", "Dist: " + String(distance) + " cm.");
   delay(500);
