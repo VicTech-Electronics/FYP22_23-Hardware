e41e814107a0d5a9da0ae4bbf1645607cd903de4
VicTech Electronics <victechelectronics@gmail.com>
2023-06-11 18:08:14 +0300

    Done

--- codes/Operation.h
+++ codes/Operation.h
@@ -9,7 +9,9 @@ const uint8_t voltage_pin=A5, switch_pin=A4, servo_pin=6;
 
 // Decralation of usefull variables
 bool setting, confirm;
-uint8_t time, size, timeAddress=0, sizeAddress=0;
+uint8_t timeAddress=0, sizeAddress=0;
+int time = 0;
+int size = 0;
 
 // Method to handle Interrupts
 void settingsInterrupt(){
@@ -56,11 +58,20 @@ void getSettings(){
 // Method to handle the whole operation
 void operation(){
   if(setting) getSettings();
-  lcdPrint("Automatic", "Fish fider");
+  lcdPrint("Automatic", "Fish feeder");
   unsigned long initial_time = millis();
-  while(millis() - initial_time > (time * 60000.)); // Wait for time to pass
+  while(millis() - initial_time >= (time * 60000.0)); // Wait for time to pass
 
-  servo.write(90); delay(10);
-  delay(map(size, 0, 100, 0, 5000));
-  servo.write(0); delay(10);
+  for(byte i=0; i<90; i++){
+    servo.write(i); 
+    delay(20);
+  }
+  
+  delay(map(size, 0, 100, 0, 10000));
+
+  for(byte i=90; i>0; i--){
+    servo.write(i); 
+    delay(20);
+  }
+  
 }
