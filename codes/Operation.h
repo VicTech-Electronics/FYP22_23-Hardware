710ff01a4b766f31dc3a15da92cae88affb79957
VicTech Electronics <victechelectronics@gmail.com>
2023-06-15 23:09:12 +0300

    To be continue

--- codes/Operation.h
+++ codes/Operation.h
@@ -4,8 +4,8 @@
 #include <SoftwareSerial.h>
 
 Servo gate;
-JSONVar jsonObject;
-SoftwareSerial serialESP(10, 11);
+JSONVar json_received_object, json_post_object;
+SoftwareSerial serialESP(11, 10);
 
 // Defintion of pin connections
 const uint8_t laser_pin=8, servo_pin=3;
@@ -25,26 +25,40 @@ void checkPacking(){
 
 // Method for posting slot status
 void postData(){
-  jsonObject["slot1Status"] = packingStatus[0];
-  jsonObject["slot2Status"] = packingStatus[1];
-  jsonObject["slot3Status"] = packingStatus[2];
-  serialESP.println(JSON.stringify(jsonObject));
+  json_post_object["slot1Status"] = packingStatus[0];
+  json_post_object["slot2Status"] = packingStatus[1];
+  json_post_object["slot3Status"] = packingStatus[2];
+  String data_to_post = JSON.stringify(json_post_object);
+  serialESP.println(json_post_object);
+
+  Serial.println("POST: " + data_to_post);
 }
 
 // MNethod to open the gate
 void openGate(){
-  gate.write(90);
-  delay(10);
-  gate.write(0);
+  Serial.println("Open the gate");
+  for(byte i=0; i<=90; i++){
+    gate.write(i);
+    delay(20);
+  }
+  delay(5e3);
+  for(byte i=90; i>=0; i--){
+    gate.write(0);
+    delay(20);
+  }
 }
 
 // Method read data
 String receiveData(){
   payload = serialESP.readString();
-  jsonObject = JSON.parse(payload);
-
-  if((int)jsonObject["packing"] == 1)
-    if(analogRead(sensorPins[0]) < calibirationValues) openGate();
-  if((int)jsonObject["payment"] == 1)
-    if(analogRead(sensorPins[1]) < calibirationValues) openGate();
+  payload.trim();
+  Serial.println("Data received: " + payload);
+  if(payload.indexOf("SUCCESS") == -1 && payload.indexOf("FAIL") == -1){
+    Serial.println("ACTION");
+    json_received_object = JSON.parse(payload);
+    if((int)json_received_object["packing"] == 1)
+      if(analogRead(sensorPins[0]) < calibirationValues) openGate();
+    if((int)json_received_object["payment"] == 1)
+      if(analogRead(sensorPins[1]) < calibirationValues) openGate();
+  }
 }
--- codes/codes.ino
+++ codes/codes.ino
@@ -2,14 +2,20 @@
 
 void setup() {
   pinMode(laser_pin, OUTPUT);
-  gate.attach(servo_pin); 
+  
   digitalWrite(laser_pin, HIGH);
+  gate.attach(servo_pin); 
+  gate.write(0);
   calibration();
+
   Serial.begin(9600);
   serialESP.begin(9600);
+
+  Serial.println("Program start");
 }
 
 void loop() {
   postData();
+  while(!serialESP.available());
   receiveData();
 }
