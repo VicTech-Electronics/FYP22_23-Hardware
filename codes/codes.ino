8c2a1e7e57895571f53019a91c0ebd07db93aae0
VicTech Electronics <victechelectronics@gmail.com>
2023-07-10 22:52:25 +0300

    Done

--- codes/codes.ino
+++ codes/codes.ino
@@ -10,13 +10,8 @@ void setup() {
   Serial.println("Initializing DFPlayer");
   if (!DFPlayer.begin(serialDFPlayer)) {  //Use softwareSerial to communicate with mp3.
     Serial.println("Fail to initialize DFPlayer");
-    while(true);
+    // while(true);
   }
-  Serial.println(F("DFPlayer Mini online."));
-  DFPlayer.setTimeOut(500); //Set serial communictaion time out 500ms
-  DFPlayer.volume(30);  //Set volume value (0~30).
-  DFPlayer.EQ(DFPLAYER_EQ_NORMAL);
-  DFPlayer.outputDevice(DFPLAYER_DEVICE_SD);
 
   Serial.println("Done");
   // Welcome messages
