e0166952e6bf78e583d6537720f57a3e5580fbe8
VicTech Electronics <victechelectronics@gmail.com>
2023-07-10 23:18:07 +0300

    Done

--- codes/codes.ino
+++ codes/codes.ino
@@ -1,13 +1,10 @@
-#include "Operation.h"
-
 void setup() {
-  pinMode(ir1_pin, INPUT);
-  pinMode(ir2_pin, INPUT);
-
   Serial.begin(9600);
-  digitalWrite(dir_pin, HIGH);
+  while(true){
+    Serial.println("Try");
+    delay(1e3);
+  }
 }
 
 void loop(){
-  operation();
 }
