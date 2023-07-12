9db28e305052e48f871eff3cf681f2005d5c6e84
VicTech Electronics <victechelectronics@gmail.com>
2023-07-12 06:36:53 +0300

    Done

--- codes/codes.ino
+++ codes/codes.ino
@@ -11,7 +11,7 @@ void setup() {
   pinMode(backlight, OUTPUT);
   pinMode(fan_pin, OUTPUT);
   pinMode(btn1_pin, INPUT_PULLUP);
-  pinMode(btn2_pin, INPUT_PULLUP);
+  pinMode(btn2_pin, INPUT_PULLUP);  
 
   attachInterrupt(digitalPinToInterrupt(btn1_pin), confirmation, FALLING);
   attachInterrupt(digitalPinToInterrupt(btn2_pin), cancelation, FALLING);
